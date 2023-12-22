152ca394f650bb61a00306734d9b599fd5974e53
udaydurgam <udaydurgam26@gmail.com>
2023-12-22 21:43:56 +0530

    sum method

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 56;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+
+    }
+}
